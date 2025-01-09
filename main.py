b707db67feb6388ab2c7e8e07221596ce9e7a5fa
KasjanK <kowalczykkasjan@gmail.com>
2025-01-09 18:02:37 +0100

    capped fps at 60 and added circleshape.py

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ from constants import *
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         screen.fill("black")
         pygame.display.flip()
+        
+        clock.tick(60)
+
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
