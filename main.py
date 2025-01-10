8faeaa4795e09e1caf795af057d3f333fc54c88b
KasjanK <kowalczykkasjan@gmail.com>
2025-01-10 16:55:07 +0100

    player can now rotate left & right

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
-    dt = 0
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        screen.fill("black")
-        pygame.display.flip()
-        
-        clock.tick(60)
-
         dt = clock.tick(60) / 1000
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+
+        pygame.display.flip()
+
+
+    
 
 if __name__ == "__main__":
     main()
