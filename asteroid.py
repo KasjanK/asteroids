cad86cb84171de6172665286f7eaff8d022fcc37
KasjanK <kowalczykkasjan@gmail.com>
2025-01-24 21:44:51 +0100

    asteroids now split when shot

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,7 @@
 from circleshape import *
+from constants import *
+
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -9,3 +12,24 @@ class Asteroid(CircleShape):
         
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        
+        vector1 = self.velocity.rotate(random_angle)
+        vector2 = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_one = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_two = Asteroid(self.position.x, self.position.y, new_radius)
+        
+        asteroid_one.velocity = vector1
+        asteroid_one.velocity *= 1.2
+        asteroid_two.velocity = vector2 
+        asteroid_two.velocity *= 1.2
